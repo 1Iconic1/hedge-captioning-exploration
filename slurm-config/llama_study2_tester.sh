f4d5ace6a80b4b38bb74b47a8e7c402e9647f4c3
Kapil Garg <kapil1garg@gmail.com>
2025-03-27 03:12:15 -0700

    adding tester slurm scripts that run only 5 examples

---
+++ slurm-config/llama_study2_tester.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --ntasks 1    		                              # Number of tasks to run
+#SBATCH --cpus-per-task=16                                # CPU cores/threads
+#SBATCH --gres=gpu:1       	                              # Number of GPUs (per node)
+#SBATCH --mem 64000        	                              # Reserve 64 GB RAM for the job
+#SBATCH --time 4-00:00    	                              # Max Runtime in D-HH:MM
+#SBATCH --partition liv.p    	                          # Partition to submit to
+#SBATCH --job-name llama_study2                           # The name of the job that is running
+#SBATCH --output /scratch/kapil/slurm-llama-study2.out    # File to which STDOUT will be written, %j inserts jobid
+#SBATCH --error /scratch/kapil/slurm-llama-study2.err  	  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodelist bird                                   # run only on birdy
+
+# activate shell
+source /home/kapilg/.local/share/virtualenvs/local-blurred-captioning-exploration-bH5G8VRE/bin/activate
+
+# go to the correct directory
+cd /home/kapilg/projects/local-blurred-captioning-exploration/scripts/
+
+# run code
+python llama_captioner.py --start 0 --end 5
+
+# deactivate environment
+exit
