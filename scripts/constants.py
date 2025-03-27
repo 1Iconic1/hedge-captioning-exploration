ff6782d85b915bc1b21acc35c1ebf63dd0b26731
Kapil Garg <kapil1garg@gmail.com>
2025-03-27 02:25:56 -0700

    adding molmo captioning script for study 2

---
+++ scripts/constants.py
@@ -0,0 +1,14 @@
+def get_prompt():
+    return "\n".join(
+        [
+            "You are a helpful assistant who describes objects in images to a blind and low-vision person. Please describe the objects in the image and any essential details necessary for identifying them.",
+            "",
+            "Follow these guidelines:",
+            "- Relevance and Specificity: Include visible text (including brand names), shapes, colors, textures, spatial relationships, or notable features only if they convey essential information about what is in the image.",
+            "- Structure of Response: Provide a description of the object with essential details. Only include details about the surrounding environment if it helps to identify the object.",
+            "- Clarity: Use simple, straightforward, objective language. Avoid unnecessary details.",
+            '- Format: Describe the object with a concise, 1-2 sentence caption. DO NOT mention camera blur or if an object is partially visible. DO NOT use "it" to refer to the object. DO NOT include statements like "The image shows" or "The object is".',
+            "",
+            "Output only the final caption.",
+        ]
+    )
--- scripts/molmo_captioner.py
+++ scripts/molmo_captioner.py
@@ -1,18 +1,19 @@
 # Libraries
 import json
-import csv
 import pandas as pd
 import os
-import re
 import copy
+import datetime
 
-import requests
-from PIL import Image
-
-from transformers import AutoModelForCausalLM, AutoProcessor, GenerationConfig
 import torch
-
+from transformers import AutoModelForCausalLM, AutoProcessor, GenerationConfig
+from PIL import Image
 from tqdm import tqdm
+
+# custom imports
+from constants import get_prompt
+from data_loader import generate_target_dataset, filter_dataset
+
 
 # setup pytorch
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # for multi-GPU systems, force single GPU
@@ -34,14 +35,14 @@ model_id = "allenai/Molmo-7B-O-0924"
 processor = AutoProcessor.from_pretrained(
     model_id,
     trust_remote_code=True,
-    torch_dtype=torch.bfloat16,
+    torch_dtype="auto",
     device_map=device_map,
 )
 
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     trust_remote_code=True,
-    torch_dtype=torch.bfloat16,
+    torch_dtype="auto",
     device_map=device_map,
 )
 
@@ -77,101 +78,24 @@ def generate_caption(
     # move inputs to the correct device and make a batch of size 1
     inputs = {k: v.to(model.device).unsqueeze(0) for k, v in inputs.items()}
 
-    # generate output; maximum 200 new tokens; stop generation when <|endoftext|> is generated
+    # generate output; maximum 300 new tokens; stop generation when <|endoftext|> is generated
     output = ""
-    with torch.autocast(device_type=device_type, enabled=True, dtype=torch.bfloat16):
-        output = model.generate_from_batch(
-            inputs,
-            GenerationConfig(max_new_tokens=200, stop_strings="<|endoftext|>"),
-            tokenizer=processor.tokenizer,
-            use_cache=False,
-            temperature=temperature,
-            do_sample=do_sample,
-        )
+    output = model.generate_from_batch(
+        inputs,
+        GenerationConfig(max_new_tokens=300, stop_strings="<|endoftext|>"),
+        tokenizer=processor.tokenizer,
+        use_cache=False,
+        temperature=temperature,
+        do_sample=do_sample,
+    )
 
-        # only get generated tokens; decode them to text
-        generated_tokens = output[0, inputs["input_ids"].size(1) :]
-        generated_text = processor.tokenizer.decode(
-            generated_tokens, skip_special_tokens=True
-        )
-        output = generated_text.strip()
+    # only get generated tokens; decode them to text
+    generated_tokens = output[0, inputs["input_ids"].size(1) :]
+    generated_text = processor.tokenizer.decode(
+        generated_tokens, skip_special_tokens=True
+    )
 
-    return output
-
-
-def generate_target_dataset(caption_dataset_filename, image_quality_dataset_filename):
-    """
-    Generates a target dataset for captioning based on VizWiz's image captioning dataset and image quality assessment dataset.
-
-    Inputs:
-    - caption_dataset_filename (str): path to caption dataset.
-    - image_quality_dataset_filename (str): path to image quality dataset.
-
-    Output:
-    - (pd.DataFrame): dataframe containing image annotations and image quality.
-    """
-    # get images and annotations in one dataframe
-    image_annotation_df = None
-    with open(caption_dataset_filename) as f:
-        # load caption dataset
-        caption_dataset_json = json.load(f)
-
-        # combine image files and annotations
-        images_df = pd.DataFrame.from_dict(caption_dataset_json["images"])
-        annotations_df = pd.DataFrame.from_dict(caption_dataset_json["annotations"])
-        grouped_annotations = (
-            annotations_df.groupby(["image_id"]).agg(tuple).map(list).reset_index()
-        )
-        image_annotation_df = images_df.merge(
-            grouped_annotations[["image_id", "caption", "is_precanned", "is_rejected"]],
-            left_on="id",
-            right_on="image_id",
-        )
-
-        # vizwiz_url is broken, so fix with https://vizwiz.cs.colorado.edu/*
-        image_annotation_df["vizwiz_url"] = image_annotation_df["vizwiz_url"].apply(
-            lambda x: x.replace(
-                "https://ivc.ischool.utexas.edu/", "https://vizwiz.cs.colorado.edu/"
-            )
-        )
-
-    # get image quality
-    with open(image_quality_dataset_filename) as f:
-        # load image quality annotation dataset
-        image_quality_dataset_json = json.load(f)
-        image_quality_df = pd.DataFrame.from_dict(image_quality_dataset_json)
-
-        # expand object of flaws into individual columns and rename
-        image_quality_df = pd.concat(
-            [
-                image_quality_df.drop(["flaws"], axis=1),
-                pd.json_normalize(image_quality_df["flaws"]),
-            ],
-            axis=1,
-        )
-        image_quality_df.rename(
-            columns={
-                "FRM": "framing",
-                "BLR": "blur",
-                "DRK": "too dark",
-                "BRT": "too bright",
-                "OBS": "obstruction",
-                "OTH": "other",
-                "NON": "no issue",
-                "ROT": "rotation",
-                "caption": "human_captions",
-            },
-            inplace=True,
-        )
-
-    # combine image and quality datasets together
-    image_captioning_input = image_annotation_df.merge(
-        image_quality_df, left_on="file_name", right_on="image"
-    ).drop(["image"], axis=1)
-
-    # convert image_captioning_input to a list of dictionaries
-    image_captioning_input = image_captioning_input.to_dict(orient="records")
-    return image_captioning_input
+    return generated_text.strip()
 
 
 def generate_caption_output(image_captioning_input, image_folder, scratch_path):
@@ -190,7 +114,7 @@ def generate_caption_output(image_captioning_input, image_folder, scratch_path):
     caption_output = copy.deepcopy(image_captioning_input)
 
     # parameters to model
-    prompt = "You are a program designed to help blind and low-vision users understand images. When asked about the image, generate accessible image description that includes key visual and contextual details of the image for blind and low-vision people. Focus on the following principles: Clarity and Conciseness: Use simple, straightforward language to describe the main subjects and their relationships.; Relevance: Highlight only essential visual elements that contribute to understanding the image or its purpose.; Context: Provide contextual information when necessary, such as emotional tone, setting, or action. Avoid assumptions or subjective interpretations.; Specificity: Include important details like colors, shapes, textures, or text visible in the image, if relevant. Avoid overly general terms or unnecessary details. Once you generate your caption, shorten it to a succinct, single sentence. Output only the final sentence. Can you please tell me what is in this image?"
+    prompt = get_prompt()
 
     # create scratch path if it doesn't exist
     os.makedirs(scratch_path, exist_ok=True)
@@ -221,18 +145,21 @@ def main():
         "../data/caption-dataset/annotations/train.json",
         "../data/image-quality-assessment/annotations/train.json",
     )
+    dataset_to_caption = filter_dataset(pd.DataFrame.from_dict(dataset_to_caption))
+    print(f"Filtered dataset of {len(dataset_to_caption)} images.")
 
     print("Generating caption output...")
     caption_output = generate_caption_output(
         dataset_to_caption,
         "../data/caption-dataset/train",
-        "../data/scratch/vizwiz-image-captions",
+        "../data/scratch/study-2/molmo-caption-output",
     )
 
     # save caption output
-    os.makedirs("../data/labeled-data/vizwiz-image-captions", exist_ok=True)
+    output_path = "../data/study-2-output/labeled-data/molmo-caption-output"
+    os.makedirs(output_path, exist_ok=True)
     with open(
-        f"../data/labeled-data/vizwiz-image-captions/caption_output_{len(caption_output)}.json",
+        f"{output_path}/caption_output_{len(caption_output)}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json",
         "w",
     ) as f:
         json.dump(caption_output, f, indent=4, separators=(",", ": "))
