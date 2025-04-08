1294d703a97595a8ea926bc81de49cdcf3c8f7f3
Kapil Garg <kapil1garg@gmail.com>
2025-04-08 11:59:17 -0700

    updating molmo captioner to use command line args and new slurm script for high-quality images

---
+++ slurm-config/molmo_study2_high-quality.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --ntasks 1    		                              # Number of tasks to run
+#SBATCH --cpus-per-task=16                                # CPU cores/threads
+#SBATCH --gres=gpu:1       	                              # Number of GPUs (per node)
+#SBATCH --mem 64000        	                              # Reserve 64 GB RAM for the job
+#SBATCH --time 4-00:00    	                              # Max Runtime in D-HH:MM
+#SBATCH --partition liv.p    	                          # Partition to submit to
+#SBATCH --job-name molmo_study2_high-quality              # The name of the job that is running
+#SBATCH --output /scratch/kapil/slurm-molmo-study2-high-quality-full.out    # File to which STDOUT will be written, %j inserts jobid
+#SBATCH --error /scratch/kapil/slurm-molmo-study2-high-quality-full.err  	  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodelist bird                                   # run only on bird
+
+# activate shell
+source /home/kapilg/.local/share/virtualenvs/local-blurred-captioning-exploration-bH5G8VRE/bin/activate
+
+# go to the correct directory
+cd /home/kapilg/projects/local-blurred-captioning-exploration/scripts/
+
+# run code
+python molmo_captioner.py \
+    --input-file "../data/study-2-input/high-quality-image_iq-1_text-no-text.json" \
+    --output-dir "../data/study-2-output/labeled-data/high-quality-images/molmo-caption-output" \
+    --scratch-path "../data/scratch/study-2/molmo-caption-output"
+
+# deactivate environment
+exit
