ccee6533e9879293a3547cb0e50bc354bb22efd0
Kapil Garg <kapil1garg@gmail.com>
2025-08-03 14:06:05 -0700

    updating slurm config for jupyter notebooks

--- slurm-config/jupyter_job.sh
+++ slurm-config/jupyter_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --ntasks 1    		                # Number of tasks to run
-#SBATCH --cpus-per-task=16                  # CPU cores/threads
+#SBATCH --cpus-per-task=32                 	# CPU cores/threads
 #SBATCH --gres=gpu:1       	                # Number of GPUs (per node)
-#SBATCH --mem 64000        	                # Reserve 64 GB RAM for the job
+#SBATCH --mem 65536        	                # Reserve 64 GB RAM for the job
 #SBATCH --time 1-00:00    	                # Max Runtime in D-HH:MM
-#SBATCH --partition liv.p    	            # Partition to submit to
-#SBATCH --output /scratch/kapil/slurm.out   # File to which STDOUT will be written, %j inserts jobid
+#SBATCH --partition liv.p    	            	# Partition to submit to
+#SBATCH --output /scratch/kapil/slurm.out   	# File to which STDOUT will be written, %j inserts jobid
 #SBATCH --error /scratch/kapil/slurm.err  	# File to which STDERR will be written, %j inserts jobid
-#SBATCH --nodelist bird                     # run only on bird
+#SBATCH --nodelist dizzy                     	# run only on dizzy
 
 # activate shell
 source /home/kapilg/.local/share/virtualenvs/local-blurred-captioning-exploration-bH5G8VRE/bin/activate
