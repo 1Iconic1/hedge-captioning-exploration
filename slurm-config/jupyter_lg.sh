c3c30b2eec245d4f7f541b81e1c5c4d1a78454d5
Kapil Garg <kapil1garg@gmail.com>
2025-08-04 21:11:07 -0700

    creating separate small and large resource jupyter slurm configs

--- slurm-config/jupyter_lg.sh
+++ slurm-config/jupyter_lg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --ntasks 1    		                # Number of tasks to run
 #SBATCH --cpus-per-task=32                 	# CPU cores/threads
-#SBATCH --gres=gpu:1       	                # Number of GPUs (per node)
-#SBATCH --mem 65536        	                # Reserve 64 GB RAM for the job
+#SBATCH --gres=gpu:2       	                # Number of GPUs (per node)
+#SBATCH --mem 262144        	                # Reserve 64 GB RAM for the job
 #SBATCH --time 1-00:00    	                # Max Runtime in D-HH:MM
 #SBATCH --partition liv.p    	            	# Partition to submit to
 #SBATCH --output /scratch/kapil/slurm.out   	# File to which STDOUT will be written, %j inserts jobid
---
+++ slurm-config/jupyter_sm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --ntasks 1    		                		# Number of tasks to run
+#SBATCH --cpus-per-task=16                 			# CPU cores/threads
+#SBATCH --gres=gpu:1       	                		# Number of GPUs (per node)
+#SBATCH --mem 65536        	                		# Reserve 64 GB RAM for the job
+#SBATCH --time 1-00:00    	                		# Max Runtime in D-HH:MM
+#SBATCH --partition liv.p    	            			# Partition to submit to
+#SBATCH --output /scratch/kapil/slurm-notebook-sm.out   	# File to which STDOUT will be written, %j inserts jobid
+#SBATCH --error /scratch/kapil/slurm-notebook-sm.err  		# File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodelist dizzy                     			# run only on dizzy
+
+# go to the correct directory
+cd /home/kapilg/projects/local-blurred-captioning-exploration/
+
+# activate shell 
+source .venv/bin/activate
+
+# run notebook
+jupyter lab --no-browser --port=9585
+
+# deactivate environment
+exit
