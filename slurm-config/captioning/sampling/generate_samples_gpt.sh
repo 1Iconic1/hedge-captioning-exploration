b9312c99c7e07e912116fcfdf90ddf3f7076255e
Kapil Garg <kapil1garg@gmail.com>
2025-10-10 00:51:59 -0700

    updating slurm scripts for sampling

--- slurm-config/captioning/sampling/generate_samples_gpt.sh
+++ slurm-config/captioning/sampling/generate_samples_gpt.sh
@@ -3,12 +3,12 @@
 #SBATCH --cpus-per-task=8                                 # CPU cores/threads
 #SBATCH --gres=gpu:0       	                              # Number of GPUs (per node)
 #SBATCH --mem 32000        	                              # Reserve 32 GB RAM for the job
-#SBATCH --time 4-00:00    	                              # Max Runtime in D-HH:MM
+#SBATCH --time 6-00:00    	                              # Max Runtime in D-HH:MM
 #SBATCH --partition liv.p    	                          # Partition to submit to
-#SBATCH --job-name generate_samples_cpu                   # The name of the job that is running
-#SBATCH --output /scratch/kapil/slurm-generate-samples-cpu.out    # File to which STDOUT will be written, %j inserts jobid
-#SBATCH --error /scratch/kapil/slurm-generate-samples-cpu.err  	  # File to which STDERR will be written, %j inserts jobid
-#SBATCH --nodelist dizzy                                   # run only on dizzy
+#SBATCH --job-name generate_samples_gpt_cpu               # The name of the job that is running
+#SBATCH --output /scratch/kapil/slurm-generate-samples-gpt-cpu.out    # File to which STDOUT will be written, %j inserts jobid
+#SBATCH --error /scratch/kapil/slurm-generate-samples-gpt-cpu.err  	  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodelist bird                                   # run only on bird
 
 # go to the correct directory
 cd /home/kapilg/projects/local-blurred-captioning-exploration/
@@ -19,7 +19,7 @@ source .venv/bin/activate
 # run code
 cd chi-2026/
 python generate_samples.py \
-    --input-file ./coded-data/cleaned/final-image-sample_945-images_09-23-25.csv \
+    --input-file ./coded-data/cleaned/remaining-images_1059-images_09-23-25.csv \
     --models gpt \
     --num-samples 10 \
     --greedy-response True
---
+++ slurm-config/captioning/sampling/generate_samples_molmo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --ntasks 1    		                              # Number of tasks to run
+#SBATCH --cpus-per-task=8                                 # CPU cores/threads
+#SBATCH --gres=gpu:2       	                              # Number of GPUs (per node)
+#SBATCH --mem 32000        	                              # Reserve 32 GB RAM for the job
+#SBATCH --time 6-00:00    	                              # Max Runtime in D-HH:MM
+#SBATCH --partition liv.p    	                          # Partition to submit to
+#SBATCH --job-name generate_samples_molmo                 # The name of the job that is running
+#SBATCH --output /scratch/kapil/slurm-generate-samples-molmo.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH --error /scratch/kapil/slurm-generate-samples-molmo.err   # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodelist bird                                   # run only on bird
+
+# go to the correct directory
+cd /home/kapilg/projects/local-blurred-captioning-exploration/
+
+# activate shell
+source .venv/bin/activate
+
+# run code
+cd chi-2026/
+python generate_samples.py \
+    --input-file ./coded-data/cleaned/chi-26-coded-final_09-23-25.csv \
+    --models molmo \
+    --num-samples 10 \
+    --greedy-response True
+
+# deactivate environment
+exit
